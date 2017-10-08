e2d5bdc3157f25b5d8094690feac0604181793a2
Black-Berry <32611512+Black-Berry@users.noreply.github.com>
2017-10-08 15:05:36 +0300

    add tests

--- test.py
+++ test.py
@@ -1,3 +1,20 @@
 import requests
+import unittest
 
-r = requests.get('https://api.github.com/events')
+class TestStringMethods(unittest.TestCase): 
+	def test_check200Responce(self):
+		r = requests.get('https://recoursia.by/')
+		self.assertEqual(r.status_code, 200, 'status not correct') 
+	def test_check200Responce(self):
+		r = requests.get('https://recoursia.by/vendors/')
+		self.assertEqual(r.status_code, 200, 'status not correct') 
+	def test_check200Responce(self):
+		r = requests.get('https://recoursia.by/news/')
+		self.assertEqual(r.status_code, 200, 'status not correct')
+	def test_check200Responce(self):
+		r = requests.get('https://recoursia.by/help/')
+		self.assertEqual(r.status_code, 200, 'status not correct')
+
+
+if __name__ == '__main__':
+	unittest.main()
